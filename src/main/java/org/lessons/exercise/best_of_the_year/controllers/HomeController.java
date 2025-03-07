6731340baf6790ec0a4df146d4de74eab1e7e8d6
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-03-07 18:17:26 +0100

    add model into home method and add some padding to h1 tag

--- src/main/java/org/lessons/exercise/best_of_the_year/controllers/HomeController.java
+++ src/main/java/org/lessons/exercise/best_of_the_year/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package org.lessons.exercise.best_of_the_year.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/") 
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("name", "riccardo");
         return "home";
     }
 }
