312d0534bed6133887f3bfd3117413e8d69dc234
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-03-10 23:18:13 +0100

    removed unused import

--- src/main/java/org/lessons/exercise/best_of_the_year/controllers/SongsController.java
+++ src/main/java/org/lessons/exercise/best_of_the_year/controllers/SongsController.java
@@ -1,7 +1,5 @@
 package org.lessons.exercise.best_of_the_year.controllers;
 import java.util.ArrayList;
-
-import org.lessons.exercise.best_of_the_year.classes.Movie;
 import org.lessons.exercise.best_of_the_year.classes.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
